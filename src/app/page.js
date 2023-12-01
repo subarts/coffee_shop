cc5f9308a29dff5b9fffeefc51be0d4f268138c4
artem subbotin <subbota1991@gmail.com>
2023-12-01 22:36:55 +0400

    feat: add coffeeSlider section

--- src/app/page.js
+++ src/app/page.js
@@ -1,47 +1,14 @@
 import Header from "@/components/Header"
 import Enjoy from "@/components/Enjoy"
 import Image from "next/image";
+import CoffeeSlider from "@/components/CoffeeSlider";
 export default function Home() {
   return (
     <>
     <Header/>
     <main className="flex min-h-screen flex-col items-center ">
       <Enjoy/>
-      <section className=" w-[85rem] h-[49.9375rem] flex flex-col items-center mt-[6.25rem]">
-      <h2 className="text-6xl leading-[125%] font-semibold">Choose your <span className="captionItalic">favorite</span> coffee</h2>
-      <div className="h-[40rem] w-[99.4%] mt-10 flex flex-row items-center justify-between">
-      <div className="roundAboutArrow">
-      <Image className="rotate-180"
-        src="/icons/arrow-right.svg"
-        alt="arrow left"
-        width={24}
-        height={24}
-        >
-        </Image>
-        </div>
-        <div className="flex items-center flex-col">
-        <Image
-          src="/image/coffee-slider-1.svg"
-          alt="logo"
-          width={480}
-          height={480}>
-        </Image>
-        <h3 className="text-2xl leading-[125%] font-semibold">S’mores Frappuccino</h3>
-        <p>This new drink takes an espresso and mixes it with brown<br/>
-         sugar and cinnamon before being topped with oat milk.</p>
-        <p>$5.50</p>
-      </div>
-      <div className="roundAboutArrow">
-      <Image 
-        src="/icons/arrow-right.svg"
-        alt="arrow left"
-        width={24}
-        height={24}
-        >
-        </Image>
-        </div>
-        </div>
-      </section>
+      <CoffeeSlider/>
     </main>
     </>
   )
---
+++ src/components/CoffeeSlider.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import Image from "next/image";
+const CoffeeSlider = () => {
+  return (
+    <section className=" w-[85rem] h-[49.9375rem] flex flex-col items-center mt-[6.25rem]">
+      <h2 className="text-6xl leading-[125%] font-semibold">
+        Choose your
+        <span className="captionItalic"> favorite </span>
+        coffee
+      </h2>
+      <div className="h-[40rem] w-[99.4%] mt-10 flex flex-row items-center justify-between">
+        <div className="roundAboutArrow">
+          <Image
+            className="rotate-180"
+            src="/icons/arrow-right.svg"
+            alt="arrow left"
+            width={24}
+            height={24}
+          ></Image>
+        </div>
+        <div className="flex items-center flex-col h-[40rem]">
+          <Image
+            src="/image/coffee-slider-1.svg"
+            alt="logo"
+            width={480}
+            height={480}
+          ></Image>
+          <div className="flex flex-col items-center gap-[1.31rem]">
+            <h3 className="text-2xl leading-[125%] font-semibold">
+              S’mores Frappuccino
+            </h3>
+            <p className="gap-1">
+              This new drink takes an espresso and mixes it with brown
+              <br />
+              &nbsp; sugar and cinnamon before being topped with oat milk.
+            </p>
+            <p className="font-semibold leading-[125%] text-2xl">$5.50</p>
+          </div>
+        </div>
+        <div className="roundAboutArrow">
+          <Image
+            src="/icons/arrow-right.svg"
+            alt="arrow left"
+            width={24}
+            height={24}
+          ></Image>
+        </div>
+      </div>
+      <div className="sliderList mt-6">
+        <div className="sliderLine"></div>
+        <div className="sliderLine sliderDisabled"></div>
+        <div className="sliderLine sliderDisabled"></div>
+      </div>
+    </section>
+  );
+};
+export default CoffeeSlider;
